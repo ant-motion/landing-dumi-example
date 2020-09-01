fdace0af7e5c60c5340361faef094c558737b38e
jljsj <liangjun.jiang@alipay.com>
2020-09-01 15:26:00 +0800

    add dynamicRoot

--- .umirc.ts
+++ .umirc.ts
@@ -5,6 +5,10 @@ export default defineConfig({
   favicon: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
-  mode: 'site'
+  mode: 'site',
+  exportStatic: {
+    htmlSuffix: true,
+    dynamicRoot: true,
+  },
   // more config: https://d.umijs.org/config
 });
